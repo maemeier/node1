b4efe8842b51ce4e4b5d31fa4816c1573e84ac09
maemeier <maemeier@live.com>
2019-06-14 16:37:08 +0700

    products

---
+++ products.js
@@ -0,0 +1,33 @@
+const _ = require('./lodash.js')
+
+let products = [
+  {
+    name: "ipad",
+    price: 200,
+    iSBN: "1"
+  },
+  {
+    name: "macbook",
+    price: 1200,
+    iSBN: "2"
+  },
+  {
+    name: "iphone",
+    price: 780,
+    iSBN: "1"
+  },
+  {
+    name: "watch",
+    price: 540,
+    iSBN: "1"
+  }
+]
+//
+let order_price = _.orderBy(products, "price")
+console.log("orderByPrice", order_price)
+
+let extract_unique = _.uniq(products,"iSBN")
+console.log("extract_unique", extract_unique)
+
+let sum = _.sumBy(products, 'price');
+console.log("sum", sum)
